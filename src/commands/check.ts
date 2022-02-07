33adf40d649a61377ffb53ec5d1f8c40f65578fa
Patrick Organ <patrick@permafrost.dev>
2022-02-07 18:05:08 -0500

    run checks async and display spinners for each check

--- src/commands/check.ts
+++ src/commands/check.ts
@@ -1,52 +1,170 @@
-import { readFileSync } from 'fs';
+import { MERAKI_API_SERVICE_URL } from '@/lib/constants';
 import axios from 'axios';
 import chalk from 'chalk';
+import ora from 'ora';
+import { readFileSync } from 'fs';
+import { encode as base64_encode } from 'js-base64';
 import { basename } from 'path';
-import { MERAKI_API_SERVICE_URL } from '@/lib/constants';
+import UpdateManager from 'stdout-update';
 
-async function runGeneralChecks(filename, code) {
-    const { data } = await axios.post(`${MERAKI_API_SERVICE_URL}/checks/general`, {
-        filename: basename(filename),
-        code: JSON.stringify(code),
-    });
+let apiToken = '';
+
+async function getApiKey() {
+    const { data } = await axios.get(`${MERAKI_API_SERVICE_URL}/auth/token`);
+
+    return data.token;
+}
+
+async function getGeneralCheckNames() {
+    const token = apiToken;
+    const { data } = await axios.get(`${MERAKI_API_SERVICE_URL}/checks/names`, { headers: { 'x-meraki-token': token } });
+
+    return data.checks.map(check => check.label);
+}
+
+async function execGeneralChecks(filename, code) {
+    const token = apiToken;
+    const { data } = await axios.post(
+        `${MERAKI_API_SERVICE_URL}/checks/general`,
+        {
+            filename: basename(filename),
+            code: base64_encode(code),
+        },
+        { headers: { 'x-meraki-token': token } },
+    );
+
+    const results: any[] = [];
 
     for (const check of data.checks) {
         if (check.skipped) {
-            process.stdout.write(`${chalk.gray('🚫')} ${check.label} ${check.message ?? ''} ${chalk.gray('(skipped)')}\n`);
+            results.push({
+                check,
+                sym: '🚫',
+                line: `${chalk.gray('🚫')} ${check.label} ${check.message ?? ''} ${chalk.gray('(skipped)')}`,
+            });
         }
 
         if (check.passed) {
-            process.stdout.write(`${chalk.greenBright('✓')} ${check.label} ${check.message ?? ''}\n`);
+            results.push({ check, sym: '✓', line: `${chalk.greenBright('✓')} ${check.label} ${check.message ?? ''}` });
         }
 
         if (!check.passed && !check.skipped) {
-            process.stdout.write(`${chalk.redBright('✗')} ${check.label} ${check.message ?? ''}\n`);
+            results.push({ check, sym: '✗', line: `${chalk.redBright('✗')} ${check.label} ${check.message ?? ''}` });
         }
     }
+
+    return results;
 }
 
-async function runP5Checks(filename, code) {
-    const { data } = await axios.post(`${MERAKI_API_SERVICE_URL}/checks/p5`, {
-        filename: basename(filename),
-        code: JSON.stringify(code),
+function formatMessage(str: string): string {
+    const re = new RegExp('`([^`]+)`', 'g');
+
+    return str.replaceAll(re, chalk.hex('#c2410c')('$1'));
+}
+
+async function execP5Checks(filename, code) {
+    const token = apiToken;
+    const { data } = await axios.post(
+        `${MERAKI_API_SERVICE_URL}/checks/p5`,
+        {
+            filename: basename(filename),
+            code: base64_encode(code),
+        },
+        { headers: { 'x-meraki-token': token } },
+    );
+
+    const results: any[] = [];
+
+    Object.values(data.errors).forEach((item: any) => {
+        results.push({ kind: 'p5error', line: `${chalk.redBright('‼️')} ${formatMessage(item.message ?? 'Unspecified error')}` });
     });
 
-    for (const check of data.errors) {
-        process.stdout.write(`${chalk.redBright('‼️')} ${check.name} ${check.message ?? ''}\n`);
-    }
+    Object.values(data.warnings).forEach((item: any) => {
+        results.push({ kind: 'p5warn', line: `${chalk.hex('#f59e0b')('⚠️')} ${formatMessage(item.message ?? 'Unspecified warning')}` });
+    });
 
-    for (const check of data.warnings) {
-        process.stdout.write(`${chalk.hex('#f59e0b')('⚠️')} ${check.name}: ${check.message ?? ''}\n`);
-    }
+    return results;
 }
 
 export const runScriptChecks = async filename => {
+    const spinner = ora('Running checks...').start();
+
     const code = readFileSync(filename, { encoding: 'utf8' })
         .replaceAll('export class', 'class')
         .replaceAll('import ', '//import ')
         .replaceAll('module.exports', '// module.exports')
         .replaceAll('export default', '// export default');
 
-    await runGeneralChecks(filename, code);
-    await runP5Checks(filename, code);
+    const manager = UpdateManager.getInstance();
+    let ticks = 1000;
+    let i = 0;
+    const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+
+    manager.hook();
+
+    apiToken = await getApiKey();
+
+    let names = (await getGeneralCheckNames()).map(name => ({
+        kind: 'general',
+        name,
+        check: null,
+        skipped: false,
+        passed: false,
+        line: ' {frame} ',
+    }));
+
+    let lines: any[] = [];
+    let execGeneralChecksResolved = false,
+        execP5ChecksResolved = false,
+        allResolved = false;
+
+    names.push({ kind: 'p5warn', name: 'P5 Function Warning Scan', check: null, skipped: false, passed: false, line: ' {frame} ' });
+    names.push({ kind: 'p5error', name: 'P5 Function Restricted Scan', check: null, skipped: false, passed: false, line: ' {frame} ' });
+
+    const id = setInterval(() => {
+        spinner.stop();
+        spinner.clear();
+
+        allResolved = execP5ChecksResolved && execGeneralChecksResolved;
+
+        if (--ticks < 0 || allResolved) {
+            clearInterval(id);
+            manager.update(lines.filter(line => line.replaceAll('{frame}', '').trim() !== ''));
+            manager.unhook(false);
+        } else {
+            const frame = frames[(i = ++i % frames.length)];
+
+            manager.update(names.map(item => item.line.replaceAll('{frame}', frame) + `${item.name}`));
+        }
+    }, 110);
+
+    execGeneralChecks(filename, code).then(results => {
+        setTimeout(() => {
+            for (let i = 0; i < results.length; i++) {
+                const idx = names.findIndex(item => item.name === results[i].check.label);
+                if (idx > -1) {
+                    names[idx].line = results[i].line;
+                }
+            }
+
+            execGeneralChecksResolved = true;
+            lines = names.map(item => ` ${item.line}`);
+        }, 600);
+    });
+
+    execP5Checks(filename, code).then(results => {
+        setTimeout(() => {
+            for (let i = 0; i < results.length; i++) {
+                const kind = results[i].kind;
+                const resultItems = results.filter(item => item.kind === kind);
+                const idx = names.findIndex(item => item.kind === kind);
+
+                names[idx] = resultItems;
+                names = names.flatMap(item => item);
+            }
+
+            lines = names.flatMap(item => ` ${item.line}`);
+            execP5ChecksResolved = true;
+        }, 600);
+    });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 //const { Command } = require('commander');
 import { Command } from 'commander';
 import { existsSync } from 'fs';
+import updateNotifier from 'update-notifier';
 import { runScriptChecks } from './commands/check';
+import pkg from '../package.json';
+
+updateNotifier({ pkg }).notify();
 
 const program = new Command();
 
 program.version('1.1.0').name('meraki-cli');
+
+program
+    .command('update-check')
+    .description('check for updates to meraki-cli')
+    .action(() => {
+        const notifier = updateNotifier({ pkg });
+
+        if (notifier.update) {
+            process.stdout.write(`Update available: ${notifier.update.latest}`);
+        } else {
+            process.stdout.write('No update available.');
+        }
+    });
 
 program
     .command('check <filename>')
