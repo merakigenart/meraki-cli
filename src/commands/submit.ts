02b9a90f08c1941786e0e190b1fc7b3f7175456a
Patrick Organ <patrick@permafrost.dev>
2022-02-08 17:51:16 -0500

    wip

--- src/commands/submit.ts
+++ src/commands/submit.ts
@@ -1,4 +1,7 @@
+import { createConf } from '@/lib/config';
 import axios from 'axios';
+import chalk from 'chalk';
+import { existsSync, readFileSync } from 'fs';
 import { encode } from 'js-base64';
 const prompts = require('prompts');
 
@@ -66,4 +69,54 @@ export const submitScriptViaApi = async (projectId, mainScript, traitsScript, ap
     const { data } = await axios.post('https://api.mraki.io/v1/scripts/submit', payload, { headers });
 
     return data;
+};
+
+export const submitActionHandler = async () => {
+    const filenames = [`${process.cwd()}/src/Script.js`, `${process.cwd()}/src/ScriptTraits.js`];
+
+    if (!existsSync(filenames[0])) {
+        process.stdout.write(`Error: '${filenames[0].replace(process.cwd(), '.')}' not found.\n`);
+        process.exit(1);
+    }
+
+    if (!existsSync(filenames[1])) {
+        process.stdout.write(`Error: '${filenames[1].replace(process.cwd(), '.')}' not found.\n`);
+        process.exit(1);
+    }
+
+    try {
+        const config = createConf('meraki-cli');
+
+        const { token, projectId, confirmSubmit, confirmOverwrite } = await getUserInputForSubmission(config);
+
+        if (!confirmSubmit || !confirmOverwrite) {
+            process.stdout.write(`Cancelled submission, not uploading to Meraki.\n`);
+            return;
+        }
+
+        if (!`${token}`.length || !`${projectId}`.length) {
+            process.stdout.write(`Please enter both a valid API Token and a valid Project ID.\n`);
+            process.exit(1);
+        }
+
+        config.set('lastProjectId', projectId);
+        config.set('lastToken', token);
+
+        process.stdout.write(`\n`);
+
+        process.stdout.write(`Uploading to Meraki...`);
+
+        const result = await submitScriptViaApi(projectId, readFileSync(filenames[0]), readFileSync(filenames[1]), token);
+
+        if (result.success) {
+            process.stdout.write(chalk.hex('#15803d')(`done.\n`));
+            process.stdout.write(`Project Dashboard: ${chalk.hex('$3b82f6')(result.url || '--')}\n`);
+        } else {
+            process.stdout.write(chalk.hex('#b91c1c')(`failed.\n`));
+            process.stdout.write(`Error: ${chalk.hex('#c2410c')(result.message || 'Unknown error')}\n`);
+        }
+    } catch (error: any) {
+        process.stdout.write(`Error: ${error.message}\n`);
+        process.exit(1);
+    }
 };
